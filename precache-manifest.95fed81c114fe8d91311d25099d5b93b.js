7acbfea956ed106985bb7be2e4fd0a3786389d01
supakitwongkaew <supakit.wo.61@ubu.ac.th>
2020-09-02 10:43:50 +0700

    Updates

---
+++ precache-manifest.95fed81c114fe8d91311d25099d5b93b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb0e9fe759d838ee7c2ef3a0fc22555",
+    "url": "/supakitwongkaew/tic-tac-toe.git/index.html"
+  },
+  {
+    "revision": "221197a6467d50a2a088",
+    "url": "/supakitwongkaew/tic-tac-toe.git/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "4cfc160bb3d13546e2db",
+    "url": "/supakitwongkaew/tic-tac-toe.git/static/js/2.c5010d1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/supakitwongkaew/tic-tac-toe.git/static/js/2.c5010d1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "221197a6467d50a2a088",
+    "url": "/supakitwongkaew/tic-tac-toe.git/static/js/main.29957e1a.chunk.js"
+  },
+  {
+    "revision": "0cb7b7e715af32f09293",
+    "url": "/supakitwongkaew/tic-tac-toe.git/static/js/runtime-main.d9e3993c.js"
+  }
+]);
